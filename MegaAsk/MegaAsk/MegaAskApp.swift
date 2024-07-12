509c3f3a05141d369fbe4d6bbe9b27fa8a2e7061
Grayson Ho <graysonhyc@gmail.com>
2024-07-12 17:14:50 +0200

    Setup

---
+++ MegaAsk/MegaAsk/MegaAskApp.swift
@@ -0,0 +1,18 @@
+//
+//  MegaAskApp.swift
+//  MegaAsk
+//
+//  Created by Grayson Ho on 12/07/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MegaAskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MegaAsk/MegaAsk/UI/ContentView.swift
@@ -0,0 +1,13 @@
+//
+//  ContentView.swift
+//  MegaAsk
+//
+//  Created by Grayson Ho on 12/07/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    Text("Hello World")
+}
